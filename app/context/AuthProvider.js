937376a948f615ba10b0563582367edb753e47b9
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2026-02-25 17:15:31 +0000

    logout fixed

--- app/context/AuthProvider.js
+++ app/context/AuthProvider.js
@@ -1,6 +1,6 @@
-import { useRouter } from "expo-router";
+import { useRouter, useSegments } from "expo-router";
 import { onAuthStateChanged } from "firebase/auth";
-import React, { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase/firebaseConfig";
 
 export const AuthContext = createContext(null);
@@ -8,24 +8,31 @@ export const AuthContext = createContext(null);
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [authLoaded, setAuthLoaded] = useState(false);
+
   const router = useRouter();
+  const segments = useSegments(); 
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+    const unsub = onAuthStateChanged(auth, (firebaseUser) => {
       setUser(firebaseUser);
       setAuthLoaded(true);
-
-      setTimeout(() => {
-        if (firebaseUser) {
-          router.replace("/(tabs)/home");
-        } else {
-          router.replace("/home");
-        }
-      }, 0);
     });
-
-    return unsubscribe;
+    return unsub;
   }, []);
+
+  useEffect(() => {
+    if (!authLoaded) return;
+
+    const inTabs = segments[0] === "(tabs)";
+
+    if (user && !inTabs) {
+      router.replace("/(tabs)/home");
+    }
+
+    if (!user && inTabs) {
+      router.replace("/sign-in"); 
+    }
+  }, [user, authLoaded, segments]);
 
   if (!authLoaded) return null;
 
